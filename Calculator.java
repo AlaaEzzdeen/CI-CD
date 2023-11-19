caa836f321828220127b2eb8cc9166cdd017d42c
AlaaEzzdeen <98087363+AlaaEzzdeen@users.noreply.github.com>
2023-11-19 21:26:50 +0000

    code

---
+++ Calculator.java
@@ -0,0 +1,17 @@
+public class Calculator {
+    public static void main(String[] args) {
+        
+    }
+    public static double add(double a, double b){
+        return a+b;
+    }
+    public static double sub(double a, double b){
+        return a-b;
+    }
+    public static double mul(double a, double b){
+        return a*b;
+    }
+    public static double div(double a, double b){
+        return a/b;
+    }
+}
