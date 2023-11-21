14db9a07ee56a16474494464e10768725b693ab4
SalahEddin Thabet <108817342+SEDDINT@users.noreply.github.com>
2023-11-21 18:19:05 +0300

    Update CalculatorTest.java

--- CalculatorTest.java
+++ CalculatorTest.java
@@ -7,7 +7,7 @@ public class CalculatorTest{
     @Test
     public void addTest(){
 
-            assertEquals(5, Calculator.add(2,3),0);
+            assertEquals(6, Calculator.add(2,3),0);
     }
 
     @Test
